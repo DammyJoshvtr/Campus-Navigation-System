bee75f43bd9c29d024733693fa830755eac458e1
Damilola Joshua <damilolajoshua2021@gmail.com>
2026-03-13 21:07:49 +0100

    Finished With Splah Screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,18 +1,29 @@
 import GeneralButton from "@/components/GeneralButton";
+import { LinearGradient } from "expo-linear-gradient";
 import { Image, StyleSheet, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { image } from "../constant/images";
 
 export default function Index() {
   return (
     <View className="flex-1">
+      {/* 1. Background Image (Bottom Layer) */}
       <Image
         source={image.background}
         resizeMode="cover"
         className="absolute w-full h-full"
       />
-      <View className="absolute bottom-20 w-full justify-center items-center px-4">
-        <GeneralButton title="Get Started" showIcon={false} />
-      </View>
+
+      {/* 2. Soft Dark Gradient Overlay (Middle Layer) */}
+      <LinearGradient
+        colors={["transparent", "rgba(0,0,0,0.85)"]}
+        className="absolute bottom-0 w-full h-1/2"
+      />
+
+      {/* 3. Content Container (Top Layer) */}
+      <SafeAreaView className="flex-1 justify-end items-center pb-10">
+        <GeneralButton title="Get Started" showIcon={true} />
+      </SafeAreaView>
     </View>
   );
 }
