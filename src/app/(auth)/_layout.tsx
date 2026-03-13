821ee47d098021fede0ec1878576b87578bd3dc1
Damilola Joshua <damilolajoshua2021@gmail.com>
2026-03-13 19:28:47 +0100

    added Image for backgound

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,11 +1,17 @@
 import { Stack } from "expo-router";
-import "./global.css";
+import React from "react";
+import { StyleSheet } from "react-native";
+// import { Stack }
 
-export default function RootLayout() {
+const _layout = () => {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
       <Stack.Screen name="(auth)" />
     </Stack>
   );
-}
+};
+
+export default _layout;
+
+const styles = StyleSheet.create({});
