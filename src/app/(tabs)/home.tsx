0efd71c6f78027d59d410b0c5f1576653b10633c
Damilola Joshua <damilolajoshua2021@gmail.com>
2026-03-16 21:22:41 +0100

    Tab bar Fixed

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -1,14 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const home = () => {
   return (
-    <View>
+    <SafeAreaView>
       <Text>home</Text>
-    </View>
-  )
-}
+    </SafeAreaView>
+  );
+};
 
-export default home
+export default home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
