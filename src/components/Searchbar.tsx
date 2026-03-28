304896119d30c4737fa5894dfa2a63871909ed63
Damilola Joshua <damilolajoshua2021@gmail.com>
2026-03-28 19:13:38 +0100

    Search Screen Completed

--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { TextInput, View } from "react-native";
 
-const Searchbar = ({ barText }: { barText: string }) => {
+const Searchbar = ({
+  barText,
+  onFocus,
+}: {
+  barText: string;
+  onFocus?: () => void;
+}) => {
   return (
     <View className="h-14">
       <TextInput
         placeholder={barText}
         placeholderTextColor="#808080"
         className="h-full w-full rounded-full text-black bg-gray-100 px-5 text-[16px] elevation-lg"
+        onFocus={onFocus}
       />
     </View>
   );
