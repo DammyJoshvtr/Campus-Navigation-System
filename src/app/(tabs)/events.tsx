7baf621c47efcdc1d55cfe3e17f129021b30d9e2
Damilola Joshua <damilolajoshua2021@gmail.com>
2026-03-30 15:36:22 +0100

    Created Events Cards

--- src/app/(tabs)/events.tsx
+++ src/app/(tabs)/events.tsx
@@ -1,10 +1,11 @@
 import Calendar from "@/components/Calendar";
+import EventsCard from "@/components/EventsCard";
+import { events } from "@/services/Events";
 import React from "react";
-import { StyleSheet, Text } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Events = () => {
-  
   return (
     <SafeAreaView className="flex-1 bg-white px-4 gap-y-4">
       <Text className="text-center font-home-bold text-[20px] mb-4">
@@ -12,6 +13,28 @@ const Events = () => {
       </Text>
       <Text className="font-home-bold font-lg">January</Text>
       <Calendar />
+
+      <View>
+        <FlatList
+          showsHorizontalScrollIndicator={false}
+          data={events}
+          renderItem={({ item }) => {
+            return (
+              <EventsCard
+                title={item.title}
+                description={item.description}
+                location={item.locationName}
+                date={item.date}
+                time={item.time}
+                status={item.status}
+                category={item.category}
+                organizer={item.organizer}
+              />
+            );
+          }}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      </View>
     </SafeAreaView>
   );
 };
