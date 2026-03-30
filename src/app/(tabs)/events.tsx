2a6e5ab11233dc24e26ec4fde0083d40e330b36d
Damilola Joshua <damilolajoshua2021@gmail.com>
2026-03-30 13:59:03 +0100

    Calendar UI Fixed

--- src/app/(tabs)/events.tsx
+++ src/app/(tabs)/events.tsx
@@ -1,14 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import Calendar from "@/components/Calendar";
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Events = () => {
   return (
-    <View>
-      <Text>Events</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="flex-1 bg-white px-4">
+      <Text className="text-center font-home-bold text-lg">Events</Text>
+      <Calendar />
+    </SafeAreaView>
+  );
+};
 
-export default Events
+export default Events;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
