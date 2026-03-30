74c20ac76b3f0dd0763192640d76d8cb8452aa0e
Damilola Joshua <damilolajoshua2021@gmail.com>
2026-03-30 14:17:26 +0100

    Added Event Objects in services

--- src/app/(tabs)/events.tsx
+++ src/app/(tabs)/events.tsx
@@ -4,9 +4,13 @@ import { StyleSheet, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Events = () => {
+  
   return (
-    <SafeAreaView className="flex-1 bg-white px-4">
-      <Text className="text-center font-home-bold text-lg">Events</Text>
+    <SafeAreaView className="flex-1 bg-white px-4 gap-y-4">
+      <Text className="text-center font-home-bold text-[20px] mb-4">
+        Events
+      </Text>
+      <Text className="font-home-bold font-lg">January</Text>
       <Calendar />
     </SafeAreaView>
   );
