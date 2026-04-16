0014a7d5ed06477ad2bcd8d81c222d42665e5839
Dammy Dev <damilolajoshua2021@gmail.com>
2026-04-16 18:37:35 +0100

    Signin API

--- native/src/services/api.ts
+++ native/src/services/api.ts
@@ -1,20 +1,28 @@
-// import axios from 'axios'
+import axios from 'axios'
 
-// const api = {
-//   baseUrl: "https://192.168.221.171:5000"
-// }
+interface SigninData {
+	name: String;
+	password: String;
+	email: String;
+}
 
-// const authSignin = () => {{
-// 	try {
-// 		const url = `${api.baseUrl}/auth/signin`;
+const api = {
+  baseUrl: "https://192.168.221.171:5000"
+}
+
+const authSignin = ({name, password, email}: SigninData) => {{
+	try {
+		const url = `${api.baseUrl}/auth/signin`;
 		
-// 		const res = await axios.post(url, {
-// 			name: "",
-// 			password: "",
-// 			email: "",
-// 		})
+		const res = axios.post(url, {
+			name: name,
+			password: password,
+			email: email,
+		})
 
-// 	} catch (err) {
-// 		console.log(err);
-// 	}    
-// }}
+	} catch (err) {
+		console.log(err);
+	}    
+}}
+
+export default {authSignin}
