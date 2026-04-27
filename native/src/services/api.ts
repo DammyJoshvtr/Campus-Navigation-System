9f6d726c165948a16e95dc5d912f756859b1861b
Dammy Dev <damilolajoshua2021@gmail.com>
2026-04-27 18:53:55 +0100

    ...

--- native/src/services/api.ts
+++ native/src/services/api.ts
@@ -1,54 +1,54 @@
-import axios from 'axios';
+import axios from "axios";
 
 interface SigninData {
-	email: string;
-	password: string;
+  email: string;
+  password: string;
 }
 
 interface SignupData {
-	fullname: string;
-	email: string;
-	password: string;
+  fullname: string;
+  email: string;
+  password: string;
 }
 
 const api = {
-	baseUrl: "http://192.168.231.171:5000"
-}
+  baseUrl: "http://192.168.231.171:5000",
+};
 
 const authSignup = async (data: SignupData) => {
-	try {
-		const res = await axios.post(`${api.baseUrl}/api/auth/signup`, data)
-		console.log(res.data)
-		return res.data
-	} catch (err: any) {
-		console.log("Signup Error: ", err.response?.data || err.message)
-		throw err
-	}
-}
+  try {
+    const res = await axios.post(`${api.baseUrl}/api/auth/signup`, data);
+    console.log(res.data);
+    return res.data;
+  } catch (err: any) {
+    console.log("Signup Error: ", err.response?.data || err.message);
+    throw err;
+  }
+};
 
 const authSignin = async (data: SigninData) => {
-	try {
-		const res = await axios.post(`${api.baseUrl}/api/auth/login`, data)
-		console.log(res.data)
-		return res.data
-	} catch (err: any) {
-		console.log("Signin Error: ", err.response?.data || err.message)
-		throw err
-	}
-}
+  try {
+    const res = await axios.post(`${api.baseUrl}/api/auth/login`, data);
+    console.log(res.data);
+    return res.data;
+  } catch (err: any) {
+    console.log("Signin Error: ", err.response?.data || err.message);
+    throw err;
+  }
+};
 
 const authVerifyOtp = async (email: string, otp: string) => {
-	try {
-		const res = await axios.post(`${api.baseUrl}/api/auth/verify-otp`, {
-			email,
-			otp
-		})
-		console.log(res.data)
-		return res.data
-	} catch (err: any) {
-		console.log("Verify Error: ", err.response?.data || err.message)
-		throw err
-	}
-}
+  try {
+    const res = await axios.post(`${api.baseUrl}/api/auth/verify-otp`, {
+      email,
+      otp,
+    });
+    console.log(res.data);
+    return res.data;
+  } catch (err: any) {
+    console.log("Verify Error: ", err.response?.data || err.message);
+    throw err;
+  }
+};
 
-export default { authSignup, authSignin, authVerifyOtp }
+export default { authSignup, authSignin, authVerifyOtp };
